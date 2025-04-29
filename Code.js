120d79ddfc155ff4b88d63321e3f22877a28999a
decs5000 <decs5000@gmail.com>
2025-04-29 14:36:55 -0400

    HELLO

--- Code.js
+++ Code.js
@@ -1,4 +1,40 @@
-var chicekn = 1;
-for (chicken) > 10 (
-    chicekn += 1
-)
+let gravity = 0.25;
+let bird_dy = 0;
+let score = 0;
+let game_state = "start";
+
+let gameInterval = null;
+
+let bird = document.getElementById("bird");
+let score_display = document.getElementById("score");
+let game_container = document.getElementById("game-container");
+let start_btn = document.getElementById("start-btn");
+
+document.addEventListener ("keydown", (e) => {
+    if (e.code === "Space" || e.code === "ArrowUP") {
+        if (game_state !== "Play") {
+            game_state = "Play";
+            startGame();
+        }
+
+        bird_dy = -7;
+    }
+})
+
+function applyGravity() {
+    bird_dy += gravity;
+    let birdTop = bird.offset + bird_dy;
+
+    birdTop = Math.max(birdTop, 0);
+    birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
+
+    bird.style.top = birdTop + "px";
+}
+
+function startGame() {
+    if (gameInterval !== null) return;
+
+    gameInterval = setInterval(() => {
+        applyGravity();
+    }, 10);
+}   
