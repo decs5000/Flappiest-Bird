18a12c78df5d777d2dd8075ff5479dcd03e9bc35
decs5000 <decs5000@gmail.com>
2025-04-30 09:29:29 -0400

    another commit

--- Code.js
+++ Code.js
@@ -10,38 +10,38 @@ let score_display = document.getElementById("score");
 let game_container = document.getElementById("game-container");
 let start_btn = document.getElementById("start-btn");
 
-document.addEventListener ("keydown", (e) => {
-    if (e.code === "Space" || e.code === "ArrowUP") {
-        if (game_state !== "Play") {
-            game_state = "Play";
-            startGame();
-        }
-
-        bird_dy = -7;
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" || e.code === "ArrowUP") {
+    if (game_state !== "Play") {
+      game_state = "Play";
+      startGame();
     }
-})
+
+    bird_dy = -7;
+  }
+});
 
 function applyGravity() {
-    bird_dy += gravity;
-    let birdTop = bird.offset + bird_dy;
+  bird_dy += gravity;
+  let birdTop = bird.offset + bird_dy;
 
-    birdTop = Math.max(birdTop, 0);
-    birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
+  birdTop = Math.max(birdTop, 0);
+  birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
 
-    bird.style.top = birdTop + "px";
+  bird.style.top = birdTop + "px";
 }
 
 function startGame() {
-    if (gameInterval !== null) return;
+  if (gameInterval !== null) return;
 
-    gameInterval = setInterval(() => {
-        applyGravity();
-    }, 10);
-}   
+  gameInterval = setInterval(() => {
+    applyGravity();
+  }, 10);
+}
 
-function onStartButtonClick(){
-    if (game_state !== "Play") {
-        game_state = "Play";
-        startGame();
-    }
+function onStartButtonClick() {
+  if (game_state !== "Play") {
+    game_state = "Play";
+    startGame();
+  }
 }
