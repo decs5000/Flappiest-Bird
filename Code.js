f9cd98e77151be2c378a5e7094e2cf9bf189f01d
decs5000 <decs5000@gmail.com>
2025-04-30 10:28:39 -0400

    pipes!!!!!!!!!!

--- Code.js
+++ Code.js
@@ -2,15 +2,19 @@ let gravity = 0.25;
 let bird_dy = 0;
 let score = 0;
 let game_state = "Start";
+let pipes = [];
+let pipe_gap = 250;
 
 let gameInterval = null;
 
 let bird = document.getElementById("bird");
+console.log(bird);
 let score_display = document.getElementById("score");
 let game_container = document.getElementById("game_container");
 let start_btn = document.getElementById("start-btn");
 
 document.addEventListener("keydown", (e) => {
+  console.log("clicked");
   if (e.code === "Space" || e.code === "ArrowUP") {
     if (game_state !== "Play") {
       game_state = "Play";
@@ -23,7 +27,7 @@ document.addEventListener("keydown", (e) => {
 
 function applyGravity() {
   bird_dy += gravity;
-  let birdTop = bird.offset + bird_dy;
+  let birdTop = bird.offsetTop + bird_dy;
 
   birdTop = Math.max(birdTop, 0);
   birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
@@ -33,7 +37,6 @@ function applyGravity() {
 
 function startGame() {
   if (gameInterval !== null) return;
-
   gameInterval = setInterval(() => {
     applyGravity();
   }, 10);
@@ -44,4 +47,44 @@ function onStartButtonClick() {
     game_state = "Play";
     startGame();
   }
+}
+
+//generate pipes
+function createPipe() {
+  let pipe_position =
+    Math.floor(Math.random() * (game_container.offsetHeight - pipe_gap - 100)) +
+    50;
+
+  //top pipe
+  let top_pipe = document.createElement("div");
+  top_pipe.className = "pipe";
+  top_pipe.style.height = pipe_position + "px";
+  top_pipe.style.top = "0px";
+  top_pipe.style.left = "100%";
+  game_container.appendChild(top_pipe);
+
+  //bottom pipe
+  let bottom_pipe = document.createElement("div");
+  bottom_pipe.className = "pipe";
+  bottom_pipe.style.height =
+    game_container.offsetHeight - pipe_gap - pipe_position + "px";
+  bottom_pipe.style.bottom = "0px";
+  bottom_pipe.style.left = "100%";
+  game_container.appendChild(bottom_pipe);
+
+  pipes.push(top_pipe, bottom_pipe);
+}
+
+//move pipes
+function movePipes() {
+  for (let pipe of pipes) {
+    pipe.style.left = pipe.offsetLeft - 3 + "px";
+
+    //remove pipes from screen
+    if (pipe.offsetLeft < -pipe.offsetWidth) {
+      pipe.remove();
+    }
+  }
+  //remove old pipes from array
+  pipes.filter((pipe) => pipe.offsetLeft + pipe.offsetWidth > 0);
 }
